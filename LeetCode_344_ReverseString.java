71ed403a8be139ac47749e395d4009505e3cfa07
Sathya-1006 <sathyapm1006@gmail.com>
2024-11-11 08:03:02 +0530

    Added solution for LeetCode #344 - Reverse String.

---
+++ LeetCode_344_ReverseString.java
@@ -0,0 +1,15 @@
+// LeetCode 344 - Reverse String
+// Problem: Reverse a string in-place
+
+class Solution {
+    public void reverseString(char[] s) {
+        int n = s.length;
+        
+        // Swap characters from both ends towards the center
+        for (int i = 0; i < n / 2; i++) {
+            char temp = s[i];
+            s[i] = s[n - 1 - i];
+            s[n - 1 - i] = temp;
+        }
+    }
+}
