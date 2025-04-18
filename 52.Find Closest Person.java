41dbab7e1ffb739ebeb71128f4d682e1718bc6ba
SATHYA P <sathyapm1006@gmail.com>
2025-04-18 08:15:00 +0530

    52. Find Closest Person.java

---
+++ 52.Find Closest Person.java
@@ -0,0 +1,14 @@
+//52.Find Closest Person
+class Solution {
+    public int findClosest(int x, int y, int z) {
+        if(Math.abs((z-x)) < Math.abs((z-y))){
+            return 1;
+        }
+            else if(Math.abs(z-x) == Math.abs(z-y) ){
+                return 0;
+            }
+        else{
+            return 2;
+        }
+    }
+}
