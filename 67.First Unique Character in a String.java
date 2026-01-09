058494e47c129af6986fdf09e5413c633391953f
SATHYA P <sathyapm1006@gmail.com>
2026-01-09 07:38:32 +0530

    67.FirstUniqChar method to find unique character
    Implement method to find the first unique character in a string.

---
+++ 67.First Unique Character in a String.java
@@ -0,0 +1,19 @@
+class Solution {
+    public int firstUniqChar(String s) {
+        //convert to charater array & check frequency
+        int freq[]=new int[256];
+        for(char c : s.toCharArray()){
+            freq[c]++;
+        }
+
+        //check the occurence
+        for(int i=0;i<s.length();i++){
+            if(freq[s.charAt(i)] == 1){
+                return i;
+            }
+        }
+        return -1;
+
+        
+    }
+}
