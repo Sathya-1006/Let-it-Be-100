93817e4e905fd70bcc790a3923fe94d617dae775
SATHYA P <sathyapm1006@gmail.com>
2026-01-19 08:34:37 +0530

    78.Subtracting product and sum of digits

---
+++ 78.Subtract the Product and Sum of Digits of an Integer.java
@@ -0,0 +1,14 @@
+class Solution {
+    public int subtractProductAndSum(int n) {
+        int sum=0;
+        int pro=1;
+        while(n!=0){
+            int rem=n%10; 
+            pro*=rem;
+            sum+=rem;
+            n/=10;
+
+        }
+        return pro-sum;
+    }
+}
